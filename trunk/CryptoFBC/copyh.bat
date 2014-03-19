f1b192c4493535ca37a9c61cc097788eaa89c3f4
cnbragon@gmail.com <cnbragon@gmail.com@f296d2d8-7def-11de-9693-657164a66812>
2014-03-19 11:23:24 +0000

    re-arrange project

--- trunk/CryptoFBC/copyh.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-copy /Y *.h "D:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\include\CryptoFBC\*.h"
-copy /Y Release\CryptoFBC.lib "D:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\lib\cryptofbcvc8.lib"
-copy /Y Debug\CryptoFBC.lib "D:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\lib\cryptofbcvc8D.lib"
